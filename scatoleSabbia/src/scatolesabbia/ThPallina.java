ce57b6d515d7020bee16744e9addddce20bb42d5
orsenigo_giacomo <giacomo.orsenigo@gmail.com>
2019-05-09 12:00:40 +0200

    Ristabilita funzionalita datiCondivisi
    Fatto da Colombo e Orsenigo

--- scatoleSabbia/src/scatolesabbia/ThPallina.java
+++ scatoleSabbia/src/scatolesabbia/ThPallina.java
@@ -8,14 +8,13 @@ package scatolesabbia;
 import java.awt.Point;
 
 /**
- *
  * @author peduzzi_samuele
  */
-public class ThPallina extends Thread  { 
-    
-    
+public class ThPallina extends Thread {
+
+
     private DatiCondivisi ptrDati;
-    private int idPallina; 
+    private int idPallina;
 
     public ThPallina(DatiCondivisi ptrDati) {
         this.ptrDati = ptrDati;
@@ -25,40 +24,39 @@ public class ThPallina extends Thread  {
         this.ptrDati = ptrDati;
         this.idPallina = idPallina;
     }
-    
-    
-    
+
+
     @Override
     public void run() {
-        
-        Scatola scatola = ptrDati.getScatola(idPallina);
-        
-        while(!isInterrupted()) {
-            
-           scatola.aggiornaPosPallina();
-           if (scatola.getPallina().isPresente()) {
-                    final Directions dirPallina = scatola.isPallinaControBordi();
-                    if (dirPallina != Directions.NONE) {
-                        final Scatola s = ptrDati.getScatolaAdiacente(idPallina, dirPallina);
-                        scatola.spostaPallina(s);
-                        Point nuovaPos = s.getPallina().getPosizione();
-                        switch (dirPallina) {
-                            case SOPRA:
-                            case SOTTO:
-                                nuovaPos.y = -nuovaPos.y;
-                                break;
-                            case SINISTRA:
-                            case DESTRA:
-                                nuovaPos.x = -nuovaPos.x;
-                                break;
-                        }
-                        s.getPallina().sposta(nuovaPos);
+        int r = 0, c = 0;
+        Scatola scatola = ptrDati.getScatola(r, c);
+
+        while (!isInterrupted()) {
+
+            scatola.aggiornaPosPallina(ptrDati.getInclinazioneTavoloDiGiocoX(),ptrDati.getInclinazioneTavoloDiGiocoY());
+            if (scatola.getPallina().isPresente()) {
+                final Directions dirPallina = scatola.isPallinaControBordi();
+                if (dirPallina != Directions.NONE) {
+                    final Scatola s = ptrDati.getScatolaAdiacente(r, c, dirPallina);
+                    scatola.spostaPallina(s);
+                    Point nuovaPos = s.getPallina().getPosizione();
+                    switch (dirPallina) {
+                        case SOPRA:
+                        case SOTTO:
+                            nuovaPos.y = -nuovaPos.y;
+                            break;
+                        case SINISTRA:
+                        case DESTRA:
+                            nuovaPos.x = -nuovaPos.x;
+                            break;
                     }
+                    s.getPallina().sposta(nuovaPos);
                 }
-            
+            }
+
         }
-        
+
     }
-    
-    
+
+
 }
