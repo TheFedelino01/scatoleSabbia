2cc3333198e7441ce0161de4f65bb8cf5b7636b0
Samuele Peduzzi <43852643+Curiosity48@users.noreply.github.com>
2019-05-15 15:38:36 +0200

    Aggiornamento commenti

--- scatoleSabbia/src/scatolesabbia/ThPallina.java
+++ scatoleSabbia/src/scatolesabbia/ThPallina.java
@@ -12,14 +12,42 @@ import java.awt.Point;
  */
 public class ThPallina extends Thread {
 
-
+    
+    /**
+     * @brief Puntatore alla classe conenente i dati condivisi
+     * @author Peduzzi Samuele
+     * @version 1.0
+     */
+    
     private DatiCondivisi ptrDati;
+    
+    /**
+     * @brief L'attributo rappresenta l'identificativo della pallina
+     * @author Peduzzi Samuele
+     * @version 1.0
+     */
+    
     private int idPallina;
-
+    
+    /**
+     * @brief Costruttore 
+     * @param ptrDati viene passsato il puntatore alla classe dati condivisi mediante costruttore
+     * @author Peduzzi Samuele
+     * @version 1.0
+     */
+    
     public ThPallina(DatiCondivisi ptrDati) {
         this.ptrDati = ptrDati;
     }
-
+    
+    /**
+     * @brief Costruttore 
+     * @param ptrDati viene passsato il puntatore alla classe dati condivisi mediante costruttore
+     * @param idPallina identificatore pallina
+     * @author Peduzzi Samuele
+     * @version 1.0
+     */
+    
     public ThPallina(DatiCondivisi ptrDati, int idPallina) {
         this.ptrDati = ptrDati;
         this.idPallina = idPallina;
@@ -27,13 +55,20 @@ public class ThPallina extends Thread {
 
 
     @Override
+    
+    /**
+     * @brief run threasd
+     * @author Peduzzi Samuele
+     * @version 1.0
+     */
+    
     public void run() {
         int r = 0, c = 0;
-        Scatola scatola = ptrDati.getScatola(r, c);
+        Scatola scatola = ptrDati.getScatola(r, c); //La pallina inizialmente è nella prima scatola
 
         while (!isInterrupted()) {
 
-            scatola.aggiornaPosPallina(ptrDati.getInclinazioneTavoloDiGiocoX(),ptrDati.getInclinazioneTavoloDiGiocoY());
+            scatola.aggiornaPosPallina(ptrDati.getInclinazioneTavoloDiGiocoX(),ptrDati.getInclinazioneTavoloDiGiocoY()); //viene aggiornata la posizione della pallina
             if (scatola.getPallina().isPresente()) {
                 final Directions dirPallina = scatola.isPallinaControBordi();
                 if (dirPallina != Directions.NONE) {
